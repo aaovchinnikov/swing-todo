730a35c490f44f5e6359b882f1f9e7d3e0a6aab5
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-09 18:41:25 +0300

    Removed all lines except one JLable component
    This should be enough to start UI code without doing anything else.

--- src/main/java/ru/hse/todo/swing/Main.java
+++ src/main/java/ru/hse/todo/swing/Main.java
@@ -1,23 +1,10 @@
 package ru.hse.todo.swing;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
 
 public class Main {
 	public static void main(String[] args) {
-		// Schedule a job for the event-dispatching thread:
-		// creating and showing this application's GUI.
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame("HelloWorldSwing");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				JLabel label = new JLabel("Hello World");
-				frame.getContentPane().add(label);
-				frame.pack();
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
+		new JLabel("Hello World");
+		System.out.println("Application finished.");
 	}
 }
