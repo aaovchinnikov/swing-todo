e8cf1ad89e7df9a21970f856dd6c18d0d79116a4
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-14 22:01:44 +0300

    Intermediate commit

--- src/main/java/ru/hse/todo/swing/Main.java
+++ src/main/java/ru/hse/todo/swing/Main.java
@@ -9,7 +9,6 @@ public class Main {
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
 		new SwingApp(
 			new MainWindow(),
-//			new NimbusLookAndFeel()
 			new MetalLookAndFeel()
 		).run();
 	}
---
+++ src/main/java/ru/hse/todo/swing/controls/Button.java
@@ -0,0 +1,11 @@
+package ru.hse.todo.swing.controls;
+
+import javax.swing.JButton;
+
+public final class Button {
+	private final JButton button;
+	
+	public Button() {
+		this.button = new JButton();
+	}
+}
